ffdd27eb660a90b9f1a3c97e8c4ead324bf35fb3
MuhammetD <doktor.oglu333@gmail.com>
2019-03-21 22:59:32 -0500

    keep forgetting commits

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,8 +7,9 @@ var elements = {
 };  
 	
 var game = {
-	
-	words : [ "supra","chaser","brabus","maybach","viper","pageout","audi"],
+
+    words : [ "supra","chaser","brabus","maybach","viper","pageout","audi"],
+    
 	guessesRemaining : 10,
 	wins : 0,
 	incorrectLetters : [],
