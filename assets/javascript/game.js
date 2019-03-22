b6ca3fbea989fd69c0f31fe1fbf5d2003b8d3537
MuhammetD <doktor.oglu333@gmail.com>
2019-03-21 21:51:33 -0500

    changes on js, css, html files

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+var words=["toyota", "viper", "mclaren", "mercedes", "skoda", "pageout","volvo"];
+var wordSelected= words[Math.floor(Math.random() * words.length)];
+var lettersGuessed=0
+for (i =0; i< wordSelected.length; i++){
+    
+}
+
